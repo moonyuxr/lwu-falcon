b2c58bd238bef6792b4efb6b906debac12be2d87
moonyuxr <moonyuxr@gmail.com>
2019-08-25 10:32:07 +0800

    Update 30_testh.sh

--- 30_testh.sh
+++ 30_testh.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-echo [{"endpoint": "yuxr_host", "tags": "", "timestamp": 1431349763, "metric": "test.yuxr.plugin", "value": 0.2333333, "counterType": "GAUGE", "step": 30}]
+echo [{"endpoint": "yuxr_host", "tags": "", "timestamp": 1566700500, "metric": "test.yuxr.plugin", "value": 0.2333333, "counterType": "GAUGE", "step": 30}]
